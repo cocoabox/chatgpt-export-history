240ce3a1da817a05458ca0e3c043491cfdd9870b
cocoabox <cocoa.kei@gmail.com>
2024-09-27 20:24:36 +0900

    added html save

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# chatgpt2json-bookmarklet
+![](imgs/example.jpg)
 
-Download ChatGPT chat history into JSON file with images.
+Download ChatGPT chat history into JSON or HTML file with images.
 
 - Saves user-provided images as base64 chunks
 - Retains ChatGPT markdown response
 - Downloading of DallE images might need page-reloading
 
-To use, copy the content of [main-minified.js](main-minified.js) and paste it to the URL box of a new bookmark.
+To use, copy the content of [bookmarklet.js](build/bookmarklet.js) and paste it to the URL box of a new bookmark.
 
--- main.js
+++ main.js
@@ -1,13 +1,127 @@
 (async function() {
+    const RELOAD_WAIT_MSEC = 1000;
+
+    if (typeof document === 'undefined' || typeof window === 'undefined') {
+        throw new Error('<<< this is a bookmarklet; please run it in a browser >>>');
+    }
     function sleep(msec) {
         return new Promise(resolve=>{
             setTimeout(()=>resolve(), msec);
         });
     }
+    function msgbox(msg_text, {actions=[], done_caption='Done'}={}) {
+        return new Promise(resolve=>{
+            let existing_modal = document.getElementById('custom_modal');
+            if (existing_modal) {
+                existing_modal.querySelector('button.done').click(); // Dismiss the existing modal
+            }
 
-    async function download_json_file(json_object, {filename='chatgpt.json'}={}) {
-        const jsonString = JSON.stringify(json_object, null, 4);
-        const blob = new Blob([jsonString], { type: 'application/json' });
+            let overlay = document.createElement('div');
+            overlay.style.cssText = `position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.7); z-index: 9999; `;
+            document.body.appendChild(overlay);
+
+            let modal = document.createElement('div');
+            modal.id = 'custom_modal';
+            modal.style.cssText = `position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background-color: white; border-radius: 1em; box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2); z-index: 10000; width: 300px; text-align: center;  box-shadow: 0 22px 70px 4px rgba(0, 0, 0, 0.56); `;
+
+            let message = document.createElement('div');
+            message.style.cssText = ` margin-bottom: 20px; font-size: 16px; color: #333; `;
+            message.innerText = msg_text;
+
+            let button_container = document.createElement('ul');
+            button_container.style.cssText = ` list-style: none; padding: 0; margin: 0; `;
+
+            function dismiss() {
+                modal.remove();
+                overlay.remove();
+                resolve(); 
+            }
+            const common_button_css = `padding: 10px 20px; border: none; border-radius: 2em; color: white; cursor: pointer; font-size: 16px; margin: 10px 0;`
+            for (const action of actions) {
+                let action_button = document.createElement('button');
+                action_button.innerText = action.caption;
+                action_button.style.cssText = `${common_button_css}; background-color: #007BFF;`
+                action_button.onclick = () => {
+                    action.callback();
+                    if (action.dismiss) {
+                        dismiss();
+                    }
+                };
+
+                let action_item = document.createElement('li');
+                action_item.appendChild(action_button);
+                button_container.appendChild(action_item);
+            }
+
+            let done_button = document.createElement('button');
+            done_button.className = 'done';
+            done_button.innerText = done_caption ?? 'Done';
+            done_button.style.cssText = `${common_button_css}; background-color: #4CAF50;`;
+
+            done_button.onclick = () => {
+                dismiss();
+            };
+
+            let done_item = document.createElement('li');
+            done_item.appendChild(done_button);
+            button_container.appendChild(done_item);
+
+
+            modal.appendChild(message);
+            modal.appendChild(button_container);
+            document.body.appendChild(modal);
+
+            return new Promise((resolve) => {
+                // Store the resolve function in a variable
+                modal.resolve = resolve;
+            });
+        });
+    }
+
+    function chat2html(chat, title) {
+        const html = `<!DOCTYPE html><html><head><title>${title}</title></head><body><dl>` +
+            chat.map(({from, from_user, from_bot, body}) => {
+                let dt = '';
+                let dd = '';
+                if (from_user) {
+                    dt = from;
+                    if (body.length === 1) {
+                        if (typeof body[0] === 'string') { dd = `<pre>${body[0]}</pre>`; }
+                        else if (body[0]?.mimeType?.startsWith('image/')) { dd= `<img src="${body[0].data}" />`; }
+                        else { dd = 'unknown'; }
+                    }
+                    else if (body.length > 1) {
+                        const list = body.map(b => {
+                            if (typeof b === 'string') { return `<li><pre>${b}</pre></li>`; }
+                            else if (b?.mimeType.startsWith('image/')) { return `<li><img src="${b.data}" /></li>`; }
+                            else { return '<li>unknown</li>'; }
+                        });
+                        dd = '<ul>' + list.join('') + '</ul>';
+                    }
+                }
+                else if (from_bot) {
+                    dt = from;
+                    if (body.length === 1) {
+                        if (typeof body[0] === 'string') { dd = body[0]; }
+                        else if (body[0]?.mimeType?.startsWith('image/')) { dd= `<img src="${body[0].data}" />`; }
+                        else { dd = 'unknown'; }
+                    }
+                    else if (body.length > 1) {
+                        const list = body.map(b => {
+                            if (typeof b === 'string') { return `<li>${b}></li>`; }
+                            else if (b?.mimeType.startsWith('image/')) { return `<li><img src="${b.data}" /></li>`; }
+                            else { return '<li>unknown</li>'; }
+                        });
+                        dd = '<ul>' + list.join('\n') + '</ul>';
+                    }
+
+                }
+                return `<dt>${dt}</dt><dd>${dd}</dd>`;
+            }).join('\n') + '</dl></body></html>';
+        return html;
+    }
+    async function download_file(bodyStr, {type= 'application/json', filename='chat.json'}={}) {
+        const blob = new Blob([bodyStr], { type });
         const link = document.createElement('a');
         link.href = URL.createObjectURL(blob);
         link.download = filename;
@@ -70,21 +184,13 @@
             if (!(img_tag instanceof HTMLImageElement)) {
                 return reject(new Error('Invalid image tag'));
             }
-
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
-
-            // Set canvas dimensions to match the image
             canvas.width = img_tag.width;
             canvas.height = img_tag.height;
-
-            // Draw the image onto the canvas
             context.drawImage(img_tag, 0, 0);
-
-            // Get the data URL (base64) and MIME type
             const dataUrl = canvas.toDataURL();
             const mimeType = dataUrl.split(';')[0].split(':')[1];
-
             // Resolve with the base64 data and MIME type
             resolve({
                 data: dataUrl.split(',')[1], // Base64 data
@@ -92,14 +198,15 @@
             });
         });
     }
+    function find_current_chat_btn() {
+        const match = window.location.href.match(/(\/c\/.*)$/);
+        const href = match ? match[0] : null;
+        if (! href) return ;
+        return document.querySelector(`a[href="${href}"]`);
+    }
     async function reload_chat() {
-        function find_button() {
-            const match = window.location.href.match(/(\/c\/.*)$/);
-            const href = match ? match[0] : null;
-            if (! href) return ;
-            return document.querySelector(`a[href="${href}"]`);
-        }
-        const button_to_return = find_button();
+
+        const button_to_return = find_current_chat_btn();
         if (! button_to_return) {
             console.warn('cannot reload because return-button not found');
             return;
@@ -111,94 +218,175 @@
             return;
         }
         main_button.click();
-        await sleep(500);
+        await sleep(RELOAD_WAIT_MSEC);
         await wait_for_element(document.body, '.composer-parent #prompt-textarea');
         console.log("going back by clicking", button_to_return);
         button_to_return.click();
-        await sleep(500);
+        await sleep(RELOAD_WAIT_MSEC);
         await wait_for_element(document.body, 'main .h-full article', {timeout:5000});
     }
 
-
-    // main
-    if (document.querySelector('.group\\/dalle-image')) {
-        if(window.confirm(
-        'Reload chat?\n\nFound some DALL-E images in your chat. DALL-E images are protected and might not download well without reloading.\n\nOK to reload now?'
-        )) { 
-            await reload_chat();
+    async function save_chat() {
+        const articles = document.querySelectorAll('main .h-full article');
+        if (! articles) {
+            console.warn('no chat history found');
+            return;
         }
-    }
-    const articles = document.querySelectorAll('main .h-full article');
-    if (! articles) {
-        alert('no chat history found');
-        return false;
-    }
-    articles[0].parentElement.parentElement.scrollTo(0,0);
-    const chat = [];
-    const articles_arr = Array.from(articles);
-    const tot = articles_arr.length - 1;
-    let dalle_err = 0;
-    for (const [idx, article] of Object.entries(articles_arr)) {
-        console.log(`message : ${idx}/${tot}`, article);
-        await scroll_into_view(article);
-        try {
-            const msg = {
-                from:'',
-                body: [],
-            };
-            const content = await wait_for_element(article, '.text-base');
-            const h5 = article.querySelector('h5.sr-only');
-            const h6 = article.querySelector('h6.sr-only');
-            if (h5) {
-                msg.user === true;
-                msg.from = h5.textContent?.match(/^(.*?):$/)?.[1] ?? h5.textContent ?? '(unknown)';
-            }
-            else if (h6) {
-                msg.chatgpt === true;
-                msg.from = h6.textContent?.match(/^(.*?):$/)?.[1] ?? h6.textContent ?? '(unknown)';
-            }
-            const dalle_images = content.querySelectorAll('.group\\/dalle-image');
-            for (const di of dalle_images) {
-                const img = di.querySelector('img');
-                if (img) {
-                    let img_content;
+        articles[0].parentElement.parentElement.scrollTo(0,0);
+        const chat = [];
+        const articles_arr = Array.from(articles);
+        const tot = articles_arr.length - 1;
+        let img_errs = [];
+        let uncaught_errs = 0;
+        for (const [idx, article] of Object.entries(articles_arr)) {
+            console.log(`message : ${idx}/${tot}`, article);
+            await scroll_into_view(article);
+            try {
+                const msg = {
+                    from:'',
+                    from_user: false,
+                    from_bot: false,
+                    body: [],
+                };
+                const content = await wait_for_element(article, '.text-base');
+                const h5 = article.querySelector('h5.sr-only');
+                const h6 = article.querySelector('h6.sr-only');
+                if (h5) {
+                    msg.from_user =true;
+                    msg.from = h5.textContent?.match(/^(.*?):$/)?.[1] ?? h5.textContent ?? '(unknown)';
+                }
+                else if (h6) {
+                    msg.from_bot =true;
+                    msg.from = h6.textContent?.match(/^(.*?):$/)?.[1] ?? h6.textContent ?? '(unknown)';
+                }
+                const dalle_images = content.querySelectorAll('.group\\/dalle-image');
+                for (const di of dalle_images) {
+                    const img = di.querySelector('img');
+                    if (img) {
+                        let img_content;
+                        try {
+                            img_content = await download_image(img.src);
+                        }
+                        catch(err) {
+                            img_errs.push('dall-e');
+                        }
+                        finally {
+                            msg.body.push(img_content);
+                        }
+                    }
+                }
+                const children = content.querySelector('[data-message-id] .w-full').children;
+                for (const child of children) {
+                    if (Array.from(child.classList).includes('markdown')) {
+                        const cloned = child.cloneNode(true);
+                        // remove buttons
+                        const buttons = cloned.getElementsByTagName('button');
+                        while (buttons.length > 0) {
+                            buttons[0].parentNode.removeChild(buttons[0]);
+                        }
+                        // Find all tags inside the clonedDiv and remove all classes
+                        for (const elem of Array.from(cloned.getElementsByTagName('*'))) {
+                            if (elem.classList.contains('text-token-text-secondary')) {
+                                if (elem.textContent?.trim())
+                                    elem.textContent = `// ${elem.textContent}`;
+                            }
+                            elem.removeAttribute('class');
+                        }
+
+                        // Convert all <code> elements to their text content
+                        const codes = cloned.getElementsByTagName('code');
+                        for (let i = codes.length - 1; i >= 0; i--) {
+                            const code = codes[i];
+                            if (code.children.length === 0) continue;
+                            // ^ e.g. <code>inlineFunc</code>
+
+                            // complicated code block <code><span class="number">123</span></code>
+                            // ↓
+                            const {textContent} = code;
+                            // const textNode = document.createTextNode(textContent);
+                            // code.parentNode.replaceChild(textNode, code);
+                            code.textContent = textContent;
+                        }
+                        msg.body.push(cloned.innerHTML.replaceAll(/<\/?div>/g, ''));
+                    }
+                    const paragraph = child.querySelector('.whitespace-pre-wrap');
+                    if (paragraph) {
+                        msg.body.push(paragraph.innerHTML);
+                    }
                     try {
-                        img_content = await download_image(img.src);
+                        const img = child.querySelector('img');
+                        if (img) {
+                            msg.body.push(await download_image(img.src));
+                        }
                     }
                     catch(err) {
-                        dalle_err++;
-                    }
-                    finally {
-                        msg.body.push(img_content);
+                        console.warn('failed to download img from', child);
+                        img_errs.push('general-img');
                     }
                 }
+                chat.push(msg);
             }
-            const children = content.querySelector('[data-message-id] .w-full').children;
-            for (const child of children) {
-                if (Array.from(child.classList).includes('markdown')) {
-                    msg.body.push(child.innerHTML);
-                }
-                const paragraph = child.querySelector('.whitespace-pre-wrap');
-                if (paragraph) {
-                    msg.body.push(paragraph.innerHTML);
-                }
-                const img = child.querySelector('img');
-                if (img) {
-                    msg.body.push(await download_image(img.src));
-                }
+            catch (error){
+                console.error(`message ${idx} failed because :`, error);
+                uncaught_errs++;
             }
-            chat.push(msg);
         }
-        catch (error){
-            console.error(`message ${idx} failed because :`, error);
+        const out = {chat, img_errs, uncaught_errs};
+        console.log("Done!", out);
+        return out;
+    }
+
+    function save_chat_wrapper() {
+        return new Promise(async resolve => {
+            let {chat, img_errs, uncaught_errs} = await save_chat() ?? {};
+            console.log("result:", {chat, img_errs, uncaught_errs});
+            if (! chat?.length) {
+                await msgbox('No chat message captured.', {done_caption:'Sorry'});
+                return false;
+            }
+            else {
+                const has_img_errs = img_errs?.length > 0;
+                const title = find_current_chat_btn()?.textContent ?? `${document.title} (untitled)`;
+                const html = chat2html(chat, title);
+                const msg = has_img_errs ? (`Some images could not be downloaded from "${title}". `
+                    +`They are protected, and usually have to be downloaded quickly after page load.`)
+                    : `"${title}" is ready for download`;
+
+                await msgbox(msg,{actions:[
+                    has_img_errs ? {
+                        dismiss:true,
+                        caption: 'Reload and Try Again',
+                        callback: () => { 
+                            console.log("will retry");
+                            return resolve('retry'); 
+                        }}:null,
+                    {caption:'Save as HTML', callback: async () => {
+                        console.log(html);
+                        await download_file(html, {type:'text/html', filename: `${document.title}.html`});
+                        resolve(true);
+                    }},
+                    {caption:'Save as JSON', callback: async () => {
+                        console.log(chat);
+                        await download_file(JSON.stringify(chat,"",4), {filename: `${document.title}.json`});
+                        resolve(true);
+                    }},
+                ].filter(n => !!n)
+                });
+            }
+        });
+    }
+
+    // main
+    while (true) {
+        const res = await save_chat_wrapper();
+        if (res === 'retry') {
+            console.log("will retry");
+            await reload_chat();
+            continue;
         }
+        break;
     }
-    if (! chat.length) {
-        window.alert('no chat history found');
-        return false;
-    }
-    else {
-        console.log('json object is ready');
-        await download_json_file(chat, {filename: `${document.title}.json`});
-    }
+
+
+
 })();
